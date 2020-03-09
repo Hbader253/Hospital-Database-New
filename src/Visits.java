3c7902fbe19644c215067a448ed6571bfea0ba55
Hbader253 <61807811+Hbader253@users.noreply.github.com>
2020-03-09 12:27:40 -0700

    Made getters and setters for visits class

--- src/Visits.java
+++ src/Visits.java
@@ -5,6 +5,37 @@ public class Visits {
 	String hospitalID = "";
 	String summary = "";	
 	String notes = "";
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getRelease() {
+		return release;
+	}
+	public void setRelease(Date release) {
+		this.release = release;
+	}
+	public String getHospitalID() {
+		return hospitalID;
+	}
+	public void setHospitalID(String hospitalID) {
+		this.hospitalID = hospitalID;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
 	//TODO add more parameters 
 	public Visits(Date date){
 		this.date = date;
