3bfe8a7446ffa548858a2c53686aefb372558e44
Hbader253 <61807811+Hbader253@users.noreply.github.com>
2020-03-09 12:21:13 -0700

    made Patient register method for finding lowest unassigned key value.
    Use this when making new register entries.

--- src/PatientRegister.java
+++ src/PatientRegister.java
@@ -3,15 +3,16 @@ import java.util.Map;
 
 public class PatientRegister {
 	Map<Integer,Patient> register = new HashMap<Integer,Patient>();	//int is patient ID number
-	
+	String title;
 	
 	public PatientRegister(String title) {	
-		
+		this.title = title;
 	}
 	
-	public int newNumber() {		//TODO find unassigned number for a new patient
-		
-		return 0;
+	public int newNumber() {		//TODO find unassigned number for a new patient	
+		for(int i = 1; true; i++) {
+			if(!register.containsKey(i)) return i;
+		}
 	}
 	
 	
